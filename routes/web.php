26bc743b19fb127b5965176f6c08ed7bd75810af
AH-Raihan <web.ahraihan@gmail.com>
2021-08-17 18:09:41 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home',function(){
-    return "hello";
-})->middleware('age');
+Route::get('/',[SiteController::class, 'showHome']);
 Route::get('/about',[SiteController::class, 'showAbout']);
 Route::get('/service',[SiteController::class, 'showservice']);
 Route::get('/portfolio',[SiteController::class, 'showPortfolio']);
