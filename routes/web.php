52368ffdd32aabee7ed8549336fc9f6e4d1bc1c7
Azizul Hasan Raihan <87574952+AH-Raihan@users.noreply.github.com>
2021-08-18 18:34:56 +0600

    hhh

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/',function(){
+return "dddd";
+});
 Route::get('/home',[SiteController::class, 'showHome']);
 Route::get('/about',[SiteController::class, 'showAbout']);
 Route::get('/service',[SiteController::class, 'showservice']);
